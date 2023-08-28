be2e0aacdc659239130a1cbf9602dc7b2fd9ad00
NithiThanakodi <race2nithi@gmail.com>
2023-08-28 15:58:48 +0530

    Updated href issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,7 @@ export const Footer = () => {
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
-                        <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
-                    </li>
-                    <li>
-                        <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Linked In</a>
-                    </li>
-                    <li>
-                        <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Youtube</a>
-                    </li>
-                    <li>
-                        <a href="#" target="_blank" className="hover:underline">Github</a>
+                        <a href="https://github.com/NithiThanakodi" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
                     </li>
                 </ul>
             </div>
